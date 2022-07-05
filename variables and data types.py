3cc7e4eeb96118c7e1dcbcd47bb3e22ca1c12dc4
MMBelyakov <bmaksimm@mail.ru>
2022-07-05 15:34:32 +0300

    Lesson 3

--- variables and data types.py
+++ variables and data types.py
@@ -1,40 +1,40 @@
 #ints
 
 number = 123412341
-print(number)
+print( number )
 
 string = "123412341"
-number = int(string)
-print(number + 5)
+number = int( string )
+print( number + 5 )
 
 
 
 number_as_string = "15"
-int(number_as_string)
+int( number_as_string )
 
 #bools
 
 bool1 = True
 bool2 = False
 
-print(bool1 , bool2)
+print( bool1 , bool2 )
 
 #floats
 a = 5.3
 b = 3.17823
-print(a + b)
+print( a + b )
 
 #dynamic_type
 
 var = ' I am a string'
 print(var)
-print(type(var))
+print( type(var) )
 
 print()
 
 var = 42
 print(var)
-print(type(var))
+print( type(var) )
 
 print()
 
@@ -49,61 +49,61 @@ y = 8
 
 result =  x + y
 print(result)
-print(x + y)
-print(x + 3)
-print(x - y )
+print( x + y )
+print( x + 3 )
+print( x - y )
 print()
-print(x * y)
-print(x / y)
-print( x // y)
-print( x % y)
+print( x * y )
+print( x / y )
+print( x // y )
+print( x % y )
 
 print()
 
-print( x ** y)
-print(x ** 0.5)
-print(pow(x,y))
+print( x ** y )
+print( x ** 0.5 )
+print( pow(x,y) )
 
 print()
 
 z = -2
-print(abs(z))
-print(z ** 2)
+print( abs(z) )
+print( z ** 2 )
 
 print()
 
-print(3.2 * 0.8 -2 * 5 - 3 ** 3)
+print( 3.2 * 0.8 -2 * 5 - 3 ** 3 )
 
 print()
 
 number=3.26
-print(round(number),round(number,1))
+print( round(number),round( number,1 ) )
 
 #math_operations
 
 import math
 x=3.265
-print('x=',x)
-print('trunc(x) = ',math.trunc(x))
-print('floor(x) = ',math.floor(x))
-print('ceil(x) = ' ,math.ceil(x))
+print( 'x=',x )
+print( 'trunc(x) = ',math.trunc(x) )
+print( 'floor(x) = ',math.floor(x) )
+print( 'ceil(x) = ' ,math.ceil(x) )
 
-print(math.pi)
-print(math.e)
+print( math.pi )
+print( math.e )
 
-print(math.sin(math.pi / 4))
+print( math.sin(math.pi / 4) )
 
-print(1/ math.sqrt(2))
+print( 1/ math.sqrt(2) )
 
 #strings
 
 string1 =  "String"
 string2 = 'String'
-print(string1,string2)
+print( string1,string2 )
 
 string = "First line of text. \n"\
           "Second line of text."
-print(string)
+print( string )
 
 multiline_string = """Some data types
 -int
@@ -112,26 +112,26 @@ multiline_string = """Some data types
 -complex
 -str
 """
-print(multiline_string)
+print( multiline_string )
 
 #print_examples
 
-print(2 , 3 , 4 , sep=' ,')
-print('he','llo',sep='')
-print('1',end=' ')
-print('2',end = '\n \n')
-print('he',end='')
-print('llo')
+print( 2 , 3 , 4 , sep=' ,' )
+print( 'he','llo',sep='' )
+print( '1',end=' ' )
+print( '2',end = '\n \n' )
+print( 'he',end='' )
+print( 'llo' )
 
 #input_examples
 string = input('Enter a string:')
-print('You have entered "{}"' .format(string))
-print('Press Enter to continue')
+print( 'You have entered "{}"' .format( string ) )
+print( 'Press Enter to continue' )
 input()
 
 m = int (input('First number: '))
 n = int (input('Second number: '))
-print  ('{} + {} = {}'.format(m,n,m+n))
+print  ( '{} + {} = {}'.format( m,n,m+n ) )
 
 
 
