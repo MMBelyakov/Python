da2aa5e8420e1e3a2ca527c31ce90f64abedd35f
MMBelyakov <bmaksimm@mail.ru>
2022-07-05 22:51:12 +0300

    Lesson 4

--- Conditional Statements.py
+++ Conditional Statements.py
@@ -2,13 +2,13 @@ number = int(input("Enter a number : "))
 if number > 5:
     print("This number is greater than five. ")
 
-if number is not None :
-    pass # add some logic here later
+if number is not None:
+    pass  # add some logic here later
 
 #if-else
-x= float(input("x = "))
+x = float(input("x = "))
 
-if x>0:
+if x > 0:
     y = x ** 0.5
 else:
     y = x ** 2
@@ -17,24 +17,24 @@ print(y)
 
 name = input("Enter your name :")
 
-if name ==  "Max" :
-    print ("You have entered " , name)
-    print ("This is my name too.")
+if name == "Max":
+    print("You have entered ", name)
+    print("This is my name too.")
 
 else:
     print("Your name differs from mine")
 
-#nested_if
+# nested_if
 x = float(input("x  = "))
-if 0 < x < 7 :
-    print("Value is in range . Let's continue")
-    y = 2 *x -5
-    if y < 0 :
-        print ("y is negative")
-    elif  y > 0 :
-            print  ("y is positive" )
+if 0 < x < 7:
+    print( "Value is in range . Let's continue")
+    y = 2 * x - 5
+    if y < 0:
+        print("y is negative")
+    elif y > 0:
+            print("y is positive")
     else:
-            print( "y = 0" )
+            print("y = 0")
 
 #elif_switch
 
@@ -45,17 +45,17 @@ print("""Menu :
 """)
 
 choice = int(input("Enter your choice : "))
-if choice ==1:
+if choice == 1:
     print("You have selected 'File' menu")
 elif choice == 2:
     print("You have selected 'View' menu")
 elif choice == 3:
     print("Stopping")
-else :
+else:
     print("Incorrect choice")
 
 #condition
-is_ready =  True
+is_ready = True
 if is_ready:
     state_msg = 'Ready'
 else:
@@ -71,10 +71,10 @@ state_msg = is_ready and 'Ready' or 'Not ready yet'
 
 string = input("Enter a string :")
 if string:
-    print('The strinf is "{}"'.format(string))
+    print('The string is "{}"'.format(string))
 
 number = int(input("Enter an integer :"))
-if number :
+if number:
     print('The number is not zero')
-else :
+else:
     print('The number is zero')
