bf1d9ddb735c4bd36f5b1466582d9944844e900b
MMBelyakov <bmaksimm@mail.ru>
2022-07-05 23:18:58 +0300

    Lesson 4

--- lists.py
+++ lists.py
@@ -1,19 +1,19 @@
-int_list = [1,2,3,5]
-char_list = ['a' , 'c' , 'x', 'z']
+int_list = [1, 2, 3, 5]
+char_list = ['a', 'c', 'x', 'z']
 empty_list = []
 
-print('Numbers' , int_list)
-print('Chars' , char_list)
+print('Numbers', int_list)
+print('Chars', char_list)
 print('empty_list', empty_list)
 
 list_from_range = list(range(5))
-print('from rangel' , list_from_range)
+print('from rangel', list_from_range)
 
 list_from_string = list("a string")
-print("from a string" , list_from_string)
+print("from a string", list_from_string)
 
-#list_indexing
-my_list = [5 ,7 ,9 ,1 ,1 ,2 ]
+# list_indexing
+my_list = [5, 7, 9, 1, 1, 2]
 print(my_list[0])
 print(my_list[1])
 
@@ -21,19 +21,19 @@ index = int(input("Enter an index : "))
 element = my_list[index]
 print(element)
 
-#negative indeces
-my_list = [5 ,7 ,9 ,1 ,1 ,2 ]
+# negative indeces
+my_list = [5, 7, 9, 1, 1, 2]
 pre_last = my_list[-2]
 print(pre_last)
 
 result = my_list[0] + my_list[-1]
 print(result)
 
-#slicing
-my_list = [5 ,7 ,9 ,1 ,1 ,2 ]
+# slicing
+my_list = [5, 7, 9, 1, 1, 2]
 
-#sub_list = my_list[:3]
-#print(sub_list)
+# sub_list = my_list[:3]
+# print(sub_list)
 
 print(my_list[2:-2])
 print(my_list[4:5])
@@ -48,22 +48,22 @@ print(my_list[2::2])
 print(my_list[:-2])
 print(my_list[::-2])
 
-#in_example
-my_list = [5 ,7 ,9 ,1 ,1 ,2 ]
+# in_example
+my_list = [5, 7, 9, 1, 1, 2]
 
 value = int(input("Enter a number : "))
 
 if value in my_list:
     print("This number is in list")
-else :
+else:
     print("This number is not in list")
 
-#len_example
+# len_example
 my_list = []
 
 print("Number of elements: ", len(my_list))
 
-#string_example
+# string_example
 string = "a string"
 print(string[0])
 print(string[2])
@@ -76,49 +76,48 @@ print(string[::-1])
 
 print(string[2] + string[-3:])
 
-#in_str
+# in_str
 
 string = input("Enter a string: ")
-if "q" in string :
-    print ("There's a 'q' in the string")
+if "q" in string:
+    print("There's a 'q' in the string")
 else:
     print("There's not a 'q' in the string")
 
-#str_len
+# str_len
 string = input("Enter a string:")
 print(len(string))
 
-#append
-my_list = [3,5]
+# append
+my_list = [3, 5]
 my_list.append((my_list[0] + my_list[1]))
 
 print(my_list)
 
-#del
-my_list = [5,3,1,35,32,0,-23]
+# del
+my_list = [5, 3, 1, 35, 32, 0, -23]
 print(my_list)
 
 del my_list[2]
 
 print(my_list)
 
-#mutation
-my_list = [5,3,1,35,32,0,-23]
+# mutation
+my_list = [5, 3, 1, 35, 32, 0, -23]
 print(my_list)
 
 my_list[3] = 18
 print(my_list)
 
-#traversal
-my_list = [5,3,1,35,32,0,-23]
+# traversal
+my_list = [5, 3, 1, 35, 32, 0, -23]
 
 for x in my_list:
-    print("{}^2 = {}".format(x , x **2 ))
+    print("{}^2 = {}".format(x, x ** 2))
 
-#fibonacci
+# fibonacci
 n = 100
-fibs = [1 , 1]
-for i in range (n - 2):
-    fibs.append(fibs[i] + fibs[i + 1 ])
+fibs = [1, 1]
+for i in range(n - 2):
+    fibs.append(fibs[i] + fibs[i + 1])
 print(fibs)
-
