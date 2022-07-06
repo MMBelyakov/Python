fd9fe4cb85692ec7ccb71e0b6181778076a1b0d4
MMBelyakov <bmaksimm@mail.ru>
2022-07-06 17:17:44 +0300

    Lesson 3

--- lists.py
+++ lists.py
@@ -89,8 +89,7 @@ string = input("Enter a string:")
 print(len(string))
 
 # append
-my_list = [3, 5]
-my_list.append((my_list[0] + my_list[1]))
+my_list = [3, 5, (my_list[0] + my_list[1])]
 
 print(my_list)
 
