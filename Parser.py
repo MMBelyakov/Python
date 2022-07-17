92763c7ba0c809545ce87d8a511a5eed1793977e
MMBelyakov <bmaksimm@mail.ru>
2022-07-17 23:56:45 +0300

    work

--- Parser.py
+++ Parser.py
@@ -1,6 +1,9 @@
 
 import re
 from datetime import datetime
+from datetime import time
+from datetime import date
+from datetime import timedelta
 
 """MESSAGE={'STATUS': 'SUCCESS', 'DATE': {'year': 2021, 'month': 12, 'day': 13, 'hour': 16, 'minute': 15},
  'TEXT': 'Подписать служебку у начальника'}"""
@@ -15,17 +18,18 @@ def main():
     Day_of_week = None
     Repeat_always = None
 
-    Status = 'Error'
+    Status = 'Success'
     print("О чём Вам напомнить ?")
     text = input()
 
     time = re.findall(r"[0-2]\d:[0-5]\d", text)
     mes = re.sub('[0-2]\d:[0-5]\d', '', text)
+    rem = re.sub('\d{2}.\d{2}.\d{4}', '', mes)
 
     match = re.search(r'\d{2}.\d{2}.\d{4}', text)
     date = datetime.strptime(match.group(), '%d.%m.%Y').date()
     day_month_year = str(date)
-    year =  day_month_year[:4]
+    year = day_month_year[:4]
     month = day_month_year[5:7]
     day = day_month_year[-2:]
 
@@ -33,7 +37,10 @@ def main():
     time_hour = time1[2:4]
     time_minute = time1[5:7]
 
+    current_date = datetime.now()  # текущие:  day month year hour minute
+    current_date_string = current_date.strftime('%d/%m/%Y %H:%M')
 
+    print(current_date_string)
 
 
     if ('в понедельник' or 'каждый понедельник' or 'понедельник' or 'Понедельник' or 'по понедельникам') in text:
@@ -59,8 +66,10 @@ def main():
 
 
 
-    print("Message:{", "'STATUS':", Status, ",", "'DATE':{'year':", year, ",", "'month':", month, ",", "'day':", day, ",", "'hour':", time_hour, ",", "'minute':", time_minute, "}", ",", "'TEXT':", mes, "}", sep='')
+    print("Message:{", "'STATUS':", Status, ",", "'DATE':{'year':", year, ",", "'month':", month, ",", "'day':", day, ",", "'hour':", time_hour, ",", "'minute':", time_minute, "}", ",", "'TEXT':",rem, "}", sep='')
 
+    #time.sleep(delta)
+    print(rem)
 if __name__ == '__main__':
     main()
 
